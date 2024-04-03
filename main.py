f36efefe28cc7444560d3a20d20c6115c00a710c
nanotechdave <davide.pilati@gmail.com>
2024-04-03 17:48:10 +0200

    Initial Upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import logging
+
+def main():
+    logging.basicConfig(
+        level = logging.DEBUG,
+        format = "%(asctime)s %(levelname)s %(message)s",
+        datefmt = "%Y-%m-%d %H:%M:%S",
+        filename = "basic.log",
+    )
+    logging.debug("this is a debug message")
+    logging.info("now we are printing info")
+    logging.warning("this is a warning")
+    logging.error("this is an error")
+    logging.critical("critical message")
+    return
+
+
+
+if __name__ == "__main__":
+    main()
+    
