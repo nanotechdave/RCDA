919edc61a7994e498863e60f61f699d3a5a714a3
nanotechdave <davide.pilati@gmail.com>
2024-04-10 15:00:57 +0200

    Refactor: utils module import in __init__

--- modules/memorycapacity.py
+++ modules/memorycapacity.py
@@ -4,10 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-import os
-import re
-
-
+from utils import find_specific_txt_files
 
 def train_test_split_time_series(data: np.array, target: np.array, test_size=0.2):
     """ Splits data and target into training and test set."""
@@ -297,31 +294,6 @@ def reformat_measurement_header(s:str) -> str:
     else:
         # Return the original string if it doesn't start with a single digit
         return s
-
-def find_specific_txt_files(folder_path: str) -> list:
-    """
-    Scans the given folder for .txt files that contain 'MemoryCapacity' in their names
-    but not 'log'.
-    
-    Parameters:
-    - folder_path: str - The path to the folder to be scanned.
-    
-    Returns:
-    - List of paths to the files that match the criteria.
-    """
-    # List all files in the directory
-    all_files = os.listdir(folder_path)
-    
-    # Compile a regular expression to match the criteria
-    pattern = re.compile(r'MemoryCapacity(?!.*log).*\.txt$', re.IGNORECASE)
-    
-    # Filter files using the regular expression
-    matching_files = [file for file in all_files if pattern.search(file)]
-    
-    # Prepend the folder path to each filename
-    full_paths = [os.path.join(folder_path, file) for file in matching_files]
-    
-    return full_paths
 
 path = "/Users/davidepilati/Library/CloudStorage/OneDrive-PolitecnicodiTorino/PhD/Misure/InrimARC/NWN_Pad130M/"
 filename = "011_INRiMARC_NWN_Pad130M_gridSE_MemoryCapacity_2024_03_28.txt"
--- utils.py
+++ utils.py
@@ -3,3 +3,27 @@ import pandas as pd
 import os
 import re
 
+def find_specific_txt_files(folder_path: str) -> list:
+    """
+    Scans the given folder for .txt files that contain 'MemoryCapacity' in their names
+    but not 'log'.
+    
+    Parameters:
+    - folder_path: str - The path to the folder to be scanned.
+    
+    Returns:
+    - List of paths to the files that match the criteria.
+    """
+    # List all files in the directory
+    all_files = os.listdir(folder_path)
+    
+    # Compile a regular expression to match the criteria
+    pattern = re.compile(r'MemoryCapacity(?!.*log).*\.txt$', re.IGNORECASE)
+    
+    # Filter files using the regular expression
+    matching_files = [file for file in all_files if pattern.search(file)]
+    
+    # Prepend the folder path to each filename
+    full_paths = [os.path.join(folder_path, file) for file in matching_files]
+    
+    return full_paths
