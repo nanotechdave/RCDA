c57df54c28658a098a89be57058ae8e2bb645742
Davide Pilati <davide.pilati@gmail.com>
2024-04-11 16:29:11 +0200

    Add: MemoryCapacity Class

--- classes/Measurement.py
+++ classes/Measurement.py
@@ -1,7 +1,12 @@
+from modules.memorycapacity import memorycapacity
 
 
 class Measurement():
     def __init__(self):
         return
     
+
+class MemoryCapacity(Measurement):
+    def __init__(self, path:str):
+        data = memorycapacity.read_and_parse_to_df(path)
     
